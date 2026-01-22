aff0a4012104077810960451be6703efe019fe4f
Kevin M <kevinlookup@gmail.com>
2026-01-21 21:01:34 -0600

    added emails

---
+++ brains.py
@@ -0,0 +1,43 @@
+import secrets
+from supabase import create_client, Client
+import os
+import dotenv
+
+import resend
+
+dotenv.load_dotenv()
+
+url = os.environ.get("SUPABASE_URL")
+key = os.environ.get("SUPABASE_KEY")
+resend.api_key = os.environ.get("RESEND_KEY")
+supabase: Client = create_client(url, key)
+
+
+def send_verification_email(email: str, code: str):
+    link = f"https://insy-project.onrender.com/verify_email/{code}"
+
+    params = {
+            "from": "onboarding@resend.dev", # Resend gives you this test email automatically
+            "to": [email],
+            "subject": "Verify your account",
+            "html": f"<p>Click this link to verify: <a href='{link}'>Verify Me</a></p>"
+        }
+
+
+    email = resend.Emails.send(params)
+    return email
+
+
+def create_user_verification(email: str):
+    code = secrets.token_urlsafe(16)
+
+    data = {
+        "email": email,
+        "code": code,
+        "is_verified": False
+        }
+
+    response = supabase.table("Users").insert(data).execute()
+
+    return code
+
