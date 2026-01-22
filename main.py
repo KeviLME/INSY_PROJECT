b5152fc7f8e3cd186807210c4af2273859f00ffa
Kevin M <kevinlookup@gmail.com>
2026-01-21 19:55:24 -0600

    combined it with suba base

--- main.py
+++ main.py
@@ -1,18 +1,27 @@
 #need two thing to run fast api
 # FASTAPI and UVICORN
 #UNVICORN is ASGI server to run FASTAPI application, in simple words it is used to serve the application
-
+import os
 from fastapi import FastAPI
+
+from supabase import create_client, Client
+
+from dotenv import load_dotenv
+load_dotenv() # take environment variables from .env.file
+
 
 app = FastAPI()
 
+url: str = os.environ.get('SUPABASE_URL')
+key: str = os.environ.get('SUPABASE_KEY')
+
+supabase: Client = create_client(url, key) # this is the cursor to interact with the database
+
 
 @app.get("/")
-async def root():
-    return {
-        'message': 'Hello World'
-    }
-
+def root():
+    response = supabase.table('Test').select("*").execute()
+    return response.data
 
 @app.get("/adding/{num1}/{num2}")
 async def add(num1: int, num2: int):
