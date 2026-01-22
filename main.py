2e9a8c9758e1b8a2e90c0eae490f9765c977c816
Kevin M <kevinlookup@gmail.com>
2026-01-21 19:17:52 -0600

    first commmit to git for insy project

---
+++ main.py
@@ -0,0 +1,21 @@
+#need two thing to run fast api
+# FASTAPI and UVICORN
+#UNVICORN is ASGI server to run FASTAPI application, in simple words it is used to serve the application
+
+from fastapi import FastAPI
+
+app = FastAPI()
+
+
+@app.get("/")
+async def root():
+    return {
+        'message': 'Hello World'
+    }
+
+
+@app.get("/adding/{num1}/{num2}")
+async def add(num1: int, num2: int):
+    return {
+        'results': num1 + num2
+    }
