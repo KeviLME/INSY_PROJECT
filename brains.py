ff22d6841617872714a9be0dc7e77c5a41da96d5
Kevin M <kevinlookup@gmail.com>
2026-01-22 19:50:50 -0600

    fixed mid template

--- brains.py
+++ brains.py
@@ -13,14 +13,51 @@ resend.api_key = os.environ.get("RESEND_KEY")
 supabase: Client = create_client(url, key)
 
 
+def get_verification_email(link_url):
+    # You can paste huge, beautiful HTML strings here
+    return f"""
+    <!DOCTYPE html>
+    <html>
+    <head>
+        <style>
+            .button {{
+                background-color: #074270;
+                color: #ffffff;
+                padding: 10px 20px;
+                text-decoration: none !important;
+                border-radius: 5px;
+            }}
+
+            .button a{{
+                color: #ffffff !important;
+                text-decoration: none;
+            }}
+        </style>
+    </head>
+    <body>
+        <h1>Welcome to MavMarket — we’re excited to have you join our community! 🎉</h1>
+        <p>Before you start exploring, please take a moment to verify your account. This helps keep MavMarket safe and ensures full access to all our features.</p>
+        <br>
+        <a href="{link_url}" class="button">Verify My Account</a>
+        <br><br>
+        <p>THANK YOU FOR JOINING US!</p>
+        <br><br>
+
+    </body>
+    </html>
+    """
+
+
+
 def send_verification_email(email: str, code: str):
     link = f"https://insy-project.onrender.com/verify_email/{code}"
+    mail = get_verification_email(link)
 
     params = {
             "from": "onboarding@resend.dev", # Resend gives you this test email automatically
             "to": [email],
-            "subject": "Verify your account",
-            "html": f"<p>Click this link to verify: <a href='{link}'>Verify Me</a></p>"
+            "subject": "MAV MARKET - Verify your account",
+            "html": mail
         }
 
 
